7ec84c055a82633c3c35d80b55fe40495f6e5254
Eirik Refsdal <eirikref@gmail.com>
2019-06-28 14:48:07 +0200

    Update deps and add sassdoc

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ class NbeatPrototype
         this.gulp.task("html",    this.gulp.series("twig"));
         this.gulp.task("css",     this.gulp.series("sass"));
         this.gulp.task("qa",      this.gulp.series("stylelint"));
+        this.gulp.task("doc",     this.gulp.series("sassdoc"));
         this.gulp.task("default", this.gulp.series("clean", "html", "css", "assets", "webserver", "watch"));
     }
     
